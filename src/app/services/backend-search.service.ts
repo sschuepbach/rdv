bd41d2297176e1e9cbae0d0506e2439c8c326580
Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>
2018-10-15 15:54:15 +0200

    Remove outdated comments
    Signed-off-by: Sebastian Schüpbach <sebastian.schuepbach@unibas.ch>

--- src/app/services/backend-search.service.ts
+++ src/app/services/backend-search.service.ts
@@ -1,20 +1,15 @@
-import { Injectable } from '@angular/core';
-import { QueryFormat } from '../models/query-format';
-import { Observable } from 'rxjs/Rx';
-import { Basket } from '../search-form/models/basket';
+import {Injectable} from '@angular/core';
+import {QueryFormat} from '../models/query-format';
+import {Observable} from 'rxjs/Rx';
+import {Basket} from '../search-form/models/basket';
 
 @Injectable({
   providedIn: 'root'
 })
 export abstract class BackendSearchService {
-
-  //Daten in Elasticsearch suchen
   abstract getBackendDataComplex(queryFormat: QueryFormat): Observable<any>;
 
-  //Detail-Daten aus Elasticsearch fuer zusaetzliche Zeile holen (abstract,...)
   abstract getBackendDetailData(id: string, fullRecord: boolean): Observable<any>;
 
-  //Merklisten-Daten in Elasticsearch suchen (ueber IDs)
   abstract getBackendDataBasket(basket: Basket): Observable<any>;
-
 }
